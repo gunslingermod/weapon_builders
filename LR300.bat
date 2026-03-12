0cad08f29f9630aaafa984f47419a2f2f251497d
gunslingermod <srkdm@yandex.ru>
2026-03-12 22:06:41 +1000

    Сборщик полуофициальной LR-300

---
+++ LR300.bat
@@ -0,0 +1,7 @@
+.\MeshCommander.exe .\wpn_lr300_unoff.script
+
+copy lr300_unoff\wpn_lr300_unoff_hud.ogf ..\gamedata\meshes\dynamics\weapons\wpn_lr300\wpn_lr300_unoff_hud.ogf
+copy lr300_unoff\wpn_lr300_unoff_hud_animation.omf ..\gamedata\meshes\dynamics\weapons\wpn_lr300\wpn_lr300_unoff_hud_animation.omf
+copy lr300_unoff\wpn_hand_lr300_hud_animation.omf ..\gamedata\meshes\dynamics\weapons\wpn_lr300\wpn_hand_lr300_hud_animation\lr300_unoff.omf
+copy lr300_unoff\wpn_lr300_unoff_world.ogf ..\gamedata\meshes\dynamics\weapons\wpn_lr300\wpn_lr300_unoff.ogf
+copy lr300_unoff\wpn_lr300_unoff_world_animation.omf ..\gamedata\meshes\dynamics\weapons\wpn_lr300\wpn_lr300_unoff_world_animation.omf
