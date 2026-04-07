b45f2fabf1cf5e4165a6ad11ac307f38ff181c6f
gunslingermod <stkdm@yandex.ru>
2026-04-08 00:44:31 +1000

    СКС

---
+++ SKS.bat
@@ -0,0 +1,7 @@
+.\MeshCommander.exe .\wpn_sks.script
+
+copy sks\wpn_sks_hud.ogf ..\gamedata\meshes\dynamics\weapons\wpn_sks\wpn_sks_hud.ogf
+copy sks\wpn_sks_hud_animation.omf ..\gamedata\meshes\dynamics\weapons\wpn_sks\wpn_sks_hud_animation.omf
+copy sks\wpn_hand_sks_hud_animation.omf ..\gamedata\meshes\dynamics\weapons\wpn_svd\wpn_hand_svd_hud_animation\wpn_hand_sks_hud_animations.omf
+copy sks\wpn_sks.ogf ..\gamedata\meshes\dynamics\weapons\wpn_sks\wpn_sks.ogf
+copy sks\wpn_sks_animation.omf ..\gamedata\meshes\dynamics\weapons\wpn_sks\wpn_sks_animation.omf
